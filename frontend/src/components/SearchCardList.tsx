6833ac68902ad1edefad11e222f67de583e6e5c5
kmin-jeong <desk535353@gmail.com>
2022-06-17 00:29:16 +0900

    fix:fix likenum

--- frontend/src/components/SearchCardList.tsx
+++ frontend/src/components/SearchCardList.tsx
@@ -14,8 +14,8 @@ const SearchCardList = ({ subData }: SearchCardListProps) => {
             id: data.id,
             houseName: data.houseName,
             houseLocation: data.houseLocation,
-            applyStartDate: data.applyStartDate || '',
-            applyEndDate: data.applyEndDate || '2022-05-23',
+            applyStartDate: data.applyStartDate,
+            applyEndDate: data.applyEndDate,
           };
 
           return (
